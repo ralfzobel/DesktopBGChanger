f4a1fcece21a2f100f8c0807da996579645a2fcd
Ralf Zobel <ralf.zobel@gmx.de>
2016-04-22 21:18:55 +0200

    now font color is dependent from brightness

--- DesktopBGChanger/src/main/java/de/acwhadk/rz/DesktopBGChanger/picture/PictureCaption.java
+++ DesktopBGChanger/src/main/java/de/acwhadk/rz/DesktopBGChanger/picture/PictureCaption.java
@@ -24,14 +24,43 @@ public class PictureCaption {
 				w, h, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = img.createGraphics();
 		g2d.drawImage(image, 0, 0, null);
-		g2d.setPaint(Color.black);
 		g2d.setFont(new Font("SansSerif", Font.BOLD, fontSize * img.getHeight() / 1024));
 		FontMetrics fm = g2d.getFontMetrics();
 		int x = left ? 10 : img.getWidth() - fm.stringWidth(text) - 10;
 		int y = top ? fm.getHeight() : img.getHeight() - fm.getHeight();
+		float luminance = getAverageLuminance(image, x, y, fm.stringWidth(text), fm.getHeight());
+		if (luminance >= 0.5f) {
+			g2d.setPaint(Color.black);
+		} else {
+			g2d.setPaint(Color.white);
+		}
 		g2d.drawString(text, x, y);
 		g2d.dispose();
 		return img;
 	}
 
+	private static float getAverageLuminance(BufferedImage image, int x0, int y0, int width, int height) {
+		int cnt=0;
+		float luminance = 0.0F;
+		for(int x=x0; x<image.getWidth(); x+=10) {
+			for(int y=y0; y<image.getHeight(); y+=10) {
+				luminance += getLuminance(image, x, y);
+				++cnt;
+			}
+		}
+		return luminance / cnt;
+	}
+	
+	private static float getLuminance(BufferedImage image, int x, int y) {
+		int color = image.getRGB(x, y);
+
+		// extract each color component
+		int red   = (color >>> 16) & 0xFF;
+		int green = (color >>>  8) & 0xFF;
+		int blue  = (color >>>  0) & 0xFF;
+
+		// calc luminance in range 0.0 to 1.0; using SRGB luminance constants
+		return (red * 0.2126f + green * 0.7152f + blue * 0.0722f) / 255;
+
+	}
 }
